7b7613e24c74ce865c133988f236c71b63ade49a
Shahnawaz Jawed <66067618+Jawed02@users.noreply.github.com>
2021-08-28 13:28:09 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,12 @@
+var start = 0;
+
+function insert(){
+document.getElementById("number").innerHTML = start +=1 ; 
+}
+function decrease(){
+ document.getElementById("number").innerHTML = start -= 1;
+}
+function reset(){
+    start = 0;
+  document.getElementById("number").innerHTML = start;
+}
