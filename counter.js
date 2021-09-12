216d94b288dc802f8f29fe4cda24860676acd510
Shahnawaz Jawed <66067618+Jawed02@users.noreply.github.com>
2021-09-12 16:21:35 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -2,12 +2,6 @@ var start = 0;
 
 function insert(){
 document.getElementById("number").innerHTML = start +=1 ;  
- if(start == 34){
- alert("Now say الحمدلله");
- }
- if(start == 67){
-alert("Now say الله أكبر");
- }
 }
 function decrease(){
  document.getElementById("number").innerHTML = start -= 1;
